de2dc95b374a60f9e11926a1b545156eb867d70a
alexandremarks <amarksweb@gmail.com>
2015-05-05 12:13:25 -0300

    Menu Principal

---
+++ functions.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+add_theme_support('menus');
+
+add_action('init', 'wp_init');
+
+function wp_init() {
+	wp_register_menus();
+}
+
+function wp_register_menus(){
+	register_nav_menu('menu-primario', 'Menu Principal');
+}
---
+++ wp_nav_menu.php
@@ -0,0 +1,25 @@
+﻿<!--Registrar menu principal -->
+
+<?php
+$defaults = array(
+    'theme_location'  => 'menu-primario',
+    'menu'            => 'Menu Principal',
+    'container'       => 'div',
+    'container_class' => 'collapse navbar-collapse',
+    'container_id'    => 'bs-example-navbar-collapse-1',
+    'menu_class'      => 'nav navbar-nav navbar-right',
+    'menu_id'         => '',
+    'echo'            => true,
+    'fallback_cb'     => 'wp_page_menu',
+    'before'          => '',
+    'after'           => '',
+    'link_before'     => '',
+    'link_after'      => '',
+    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+    'depth'           => 0,
+    'walker'          => ''
+);
+
+wp_nav_menu( $defaults );
+
+?>
